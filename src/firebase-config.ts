66634700cda33808f7c5d838be2e83752ec29d12
shubhamgh <shubhamm191200@gmail.com>
2024-12-01 02:33:06 +0530

    Excluded env variables

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -10,13 +10,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC7X4LM5AP-22mdq5hZFXpyOLrl_0vOYVY",
-  authDomain: "evryday-e78e2.firebaseapp.com",
-  projectId: "evryday-e78e2",
-  storageBucket: "evryday-e78e2.firebasestorage.app",
-  messagingSenderId: "787701707927",
-  appId: "1:787701707927:web:010b003a256e0c6a1565d2",
-  measurementId: "G-H1G90CJKKN"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId:  process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
